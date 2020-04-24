736f54ccc8df840d241e87010a8286e9c15a49ca
Marco Ris <ris@contria.com>
2020-04-24 17:51:16 +0200

    Add update file for categories

---
+++ api/category/update.php
@@ -0,0 +1,34 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-type, Access-Control-Allow-Methods, Authorization, X-Requested-Width');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+// Instantiate DB + connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Category object
+$category = new Category($db);
+
+// Get raw Categoryed data
+$data = json_decode(file_get_contents("php://input"));
+
+// Set id to update
+$category->id = $data->id;
+$category->name = $data->name;
+
+// Update Category
+if ($category->update()) {
+    echo json_encode(
+        array('message' => 'Category updated')
+    );
+} else {
+        echo json_encode(
+            array('message' => 'Category not updated')
+        );
+}
