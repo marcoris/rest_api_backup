90a2c1f173bf953e2e1c48a8b5c7e737e6f47f57
Marco Ris <ris@contria.com>
2020-04-24 17:50:51 +0200

    Add delete file for categories

---
+++ api/category/delete.php
@@ -0,0 +1,33 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-type, Access-Control-Allow-Methods, Authorization, X-Requested-Width');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+// Instantiate DB + connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Category object
+$category = new Category($db);
+
+// Get raw Categoryed data
+$data = json_decode(file_get_contents("php://input"));
+
+// Set id to update
+$category->id = $data->id;
+
+// Delete Category
+if ($category->delete()) {
+    echo json_encode(
+        array('message' => 'Category deleted')
+    );
+} else {
+        echo json_encode(
+            array('message' => 'Category not deleted')
+        );
+}
