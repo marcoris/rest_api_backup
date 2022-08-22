537722f06c43a9cc7955c4b447bc348c53d5a114
Marco Ris <ris@contria.com>
2022-08-22 23:37:21 +0200

    Prepare index for routing

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-header("Location: ../");
-die();
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT');
+
+$method = $_SERVER["REQUEST_METHOD"];
+//$requestUri = $_SERVER["REQUEST_URI"];
+echo $method;
