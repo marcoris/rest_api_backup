b524dc653d5238d04a0d65c1d2ce846d26e49d5a
Marco Ris <ris@contria.com>
2022-08-22 23:35:18 +0200

    Cleanup code

--- api/1.0/models/Post.php
+++ api/1.0/models/Post.php
@@ -1,12 +1,13 @@
 <?php
+
 class Post {
     /**
      * @var string
      */
-    private $table = 'posts';
+    private $table;
 
     /**
-     * @var object 
+     * @var object
      */
     private $conn;
 
@@ -36,31 +37,22 @@ class Post {
     public $category_id;
 
     /**
-     * @var string
-     */
-    public $category_name;
-
-    /**
-     * @var string
-     */
-    public $created_at;
-
-    /**
      * Post constructor
      * @param object $database
      */
-    public function __construct($database)
+    public function __construct(object $database)
     {
         $this->conn = $database;
+        $this->table = "posts";
     }
 
     /**
      * Get Posts
-     * 
+     *
      * @return object
      */
-    public function read()
-    {       
+    public function read(): object
+    {
         // Prepare statement
         $stmt = $this->conn->prepare(
             "SELECT
@@ -84,104 +76,148 @@ class Post {
         return $stmt;
     }
 
-    // Get single post
-    public function readSingle()
+    /**
+     * Get single post
+     *
+     * @return object|null
+     */
+    public function readSingle(): ?object
     {
-        // Prepare statement
-        $stmt = $this->conn->prepare(
-            "SELECT
-                c.name as category_name,
-                p.id,
-                p.category_id,
-                p.title,
-                p.body,
-                p.author,
-                p.created_at
-            FROM " .
-                $this->table . " p
-                LEFT JOIN categories c ON p.category_id = c.id
-            WHERE
-                p.id = :id"
-        );
+        try {
+            // Prepare statement
+            $stmt = $this->conn->prepare(
+                "SELECT
+                    c.name as category_name,
+                    p.id,
+                    p.category_id,
+                    p.title,
+                    p.body,
+                    p.author,
+                    p.created_at
+                FROM " .
+                    $this->table . " p
+                    LEFT JOIN categories c ON p.category_id = c.id
+                WHERE
+                    p.id = :id"
+            );
 
-        // Execute query
-        $stmt->execute(array(
-            ':id' => $this->post_id
-        ));
+            // Execute query
+            $stmt->execute(array(
+                ':id' => $this->post_id
+            ));
 
-        return $stmt;
-    }
-
-    // Create post
-    public function create()
-    {
-        $stmt = $this->conn->prepare(
-            "INSERT INTO " . $this->table . "
-            SET
-                title = :title,
-                body = :body,
-                author = :author,
-                category_id = :category_id"
-        );
-
-        if ($stmt->execute(array(
-            ':title' => htmlspecialchars(strip_tags($this->title)),
-            ':body' => htmlspecialchars(strip_tags($this->body)),
-            ':author' => htmlspecialchars(strip_tags($this->author)),
-            ':category_id' => htmlspecialchars(strip_tags($this->category_id))
-        ))) {
-            return true;
+            return $stmt;
+        } catch (PDOException $e) {
+            echo json_encode(
+                array('internal_error' => $e->getMessage())
+            );
         }
 
-        // Print error
-        printf("Error: %s.\n", $stmt->error);
+        return null;
+    }
+
+    /**
+     * Create post
+     *
+     * @return bool
+     */
+    public function create(): bool
+    {
+        try {
+            $stmt = $this->conn->prepare(
+                "INSERT INTO " . $this->table . "
+                SET
+                    title = :title,
+                    body = :body,
+                    author = :author,
+                    category_id = :category_id"
+            );
+
+            if ($stmt->execute(array(
+                ':title' => htmlspecialchars(strip_tags($this->title)),
+                ':body' => htmlspecialchars(strip_tags($this->body)),
+                ':author' => htmlspecialchars(strip_tags($this->author)),
+                ':category_id' => htmlspecialchars(strip_tags($this->category_id))
+            ))) {
+                $this->post_id = $this->conn->lastInsertId();
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo json_encode(
+            array('internal_error' => $e->getMessage())
+            );
+        }
 
         return false;
     }
 
-    // Update post
-    public function update()
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId(): int
     {
-        $stmt = $this->conn->prepare(
-            "UPDATE " . $this->table . "
-            SET
-                title = :title,
-                body = :body,
-                author = :author,
-                category_id = :category_id
-            WHERE
-                id = :id"
-        );
+        return $this->post_id;
+    }
 
-        if ($stmt->execute(array(
-            ':title' => htmlspecialchars(strip_tags($this->title)),
-            ':body' => htmlspecialchars(strip_tags($this->body)),
-            ':author' => htmlspecialchars(strip_tags($this->author)),
-            ':category_id' => htmlspecialchars(strip_tags($this->category_id)),
-            ':id' => htmlspecialchars(strip_tags($this->post_id))
-        ))) {
-            return true;
+    /**
+     * Update post
+     *
+     * @return bool
+     */
+    public function update(): bool
+    {
+        try {
+            $stmt = $this->conn->prepare(
+                "UPDATE " . $this->table . "
+                SET
+                    title = :title,
+                    body = :body,
+                    author = :author,
+                    category_id = :category_id
+                WHERE
+                    id = :id"
+            );
+
+            if ($stmt->execute(array(
+                ':title' => htmlspecialchars(strip_tags($this->title)),
+                ':body' => htmlspecialchars(strip_tags($this->body)),
+                ':author' => htmlspecialchars(strip_tags($this->author)),
+                ':category_id' => htmlspecialchars(strip_tags($this->category_id)),
+                ':id' => htmlspecialchars(strip_tags($this->post_id))
+            ))) {
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo json_encode(
+            array('internal_error' => $e->getMessage())
+            );
         }
-
-        // Print error
-        printf("Error: %s.\n", $stmt->error);
 
         return false;
     }
 
-    // Delete post
-    public function delete()
+    /**
+     * Delete post
+     *
+     * @return bool
+     */
+    public function delete(): bool
     {
-        $stmt = $this->conn->prepare("DELETE FROM " . $this->table . " WHERE id = :id");
-        
-        if ($stmt->execute(array(
-            ":id" => $this->post_id
-        ))) {
-            return true;
-        }
+        try {
+            $stmt = $this->conn->prepare("DELETE FROM " . $this->table . " WHERE id = :id");
 
-        // Print error
-        printf("Error: %s.\n", $stmt->error);
+            if ($stmt->execute(array(
+                ":id" => $this->post_id
+            ))) {
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo json_encode(
+                array('internal_error' => $e->getMessage())
+            );
+        }
 
         return false;
     }
