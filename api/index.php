ef5371c2064d1d3ab91936f90d45c372de346d91
Marco Ris <ris@contria.com>
2022-08-28 20:22:16 +0200

    Adjust redirecting from api directory

--- api/index.php
+++ api/index.php
@@ -1,7 +1,3 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT');
-
-$method = $_SERVER["REQUEST_METHOD"];
-//$requestUri = $_SERVER["REQUEST_URI"];
-echo $method;
+header("Location: ../");
+die();
